1b72803666d32043783646fcf9217c282f66b784
Utilizador de laboratório <lab@di.uminho.pt>
2015-03-01 11:40:43 +0000

    Adicionei as aulas e o enunciado

--- interpretador/comandos.h
+++
@@ -1,2 +0,0 @@
-int somar(char *cmd, int nargs, char **args);
-int sair(char *cmd, int nargs, char **args);
--- interpretador/erros.h
+++
@@ -1,9 +0,0 @@
-/** Identificadores dos erros */
-typedef enum {
-	SAIR,
-	ERRO_OK,
-	ERRO_CMD_NAO_EXISTE,
-	ERRO_NARGS
-} ERROS;
-
-void imprime_erro(int erro);
